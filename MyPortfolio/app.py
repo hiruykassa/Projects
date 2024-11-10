c8fca33c3c9f0f58a2a95e67926326f98b9f1044
Hiruy Kassa <hiruygirmak@gmail.com>
2024-11-10 14:18:49 -0600

    Prepare MyPortfolio for deployment

--- MyPortfolio/app.py
+++ MyPortfolio/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
@@ -18,5 +19,5 @@ def contact():
 def resume():
     return render_template('resume.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
